3e8a356a5976a8bfd25d05ef68784bae9cf89234
CJ-404 <cj.ptsz@yahoo.com>
2024-05-07 14:03:09 +0530

    create: model for project

---
+++ lib/data/models/project.dart
@@ -0,0 +1,47 @@
+import 'package:equatable/equatable.dart';
+
+class Project extends Equatable {
+  final int? id;
+  final String title;
+  final String description;
+  // final String time;
+  // final String date;
+  // final bool completed;
+
+  const Project({
+    this.id,
+    required this.title,
+    required this.description,
+    // required this.time,
+    // required this.date
+  });
+
+  factory Project.fromJson(Map<String, dynamic> json){
+    return Project(
+      id: json['id'],
+      title: json['project_name'],
+      description: json['project_description'],
+    );
+  }
+
+  // factory Project.toJson(Map<String, dynamic> json){
+  //   return Project(
+  //     id: json['id'],
+  //     title: json['project_name'],
+  //     description: json['project_description'],
+  //   );
+  // }
+
+  @override
+  // TODO: implement props
+  List<Object?> get props {
+    return [
+      id!,
+      title,
+      description,
+      // time,
+      // date,
+    ];
+  }
+
+}
