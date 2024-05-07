4a8928f2f45240bbeb68e7e75d8d7da467ae1ff0
CJ-404 <cj.ptsz@yahoo.com>
2024-05-07 14:00:43 +0530

    create: user providers

---
+++ lib/providers/user/users_provider.dart
@@ -0,0 +1,17 @@
+import 'package:flatwork/data/data.dart';
+import 'package:flatwork/providers/api_service_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../task/tasks_provider.dart';
+
+final userFilterProvider = StateProvider<String>((ref) {
+  return "";
+});
+
+final usersProvider = FutureProvider<List<User>>((ref) async {
+  return ref.watch(apiServiceProvider).getUsers();
+});
+
+// final assignedUsersProvider = FutureProvider<List<User>>((ref) async {
+//   final String taskId = ref.watch(taskIdProvider);
+//   return ref.watch(apiServiceProvider).getAssignedUsers(taskId);
+// });
