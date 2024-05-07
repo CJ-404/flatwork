be489417d7f919d495ed23ff41a2f3515e96dc76
CJ-404 <cj.ptsz@yahoo.com>
2024-05-07 13:55:25 +0530

    routes:add routes to assign team members for a project functionality

---
+++ lib/config/routes/route_location.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class RouteLocation{
+  const RouteLocation._();
+
+  static String get home => '/home';
+  static String get createProject => '/createProject';
+  static String get viewProject => '/viewProject/:projectId';
+  static String get editTask => '/editTask/:taskId';
+  static String get addTask => '/addTask/:projectId';
+}
