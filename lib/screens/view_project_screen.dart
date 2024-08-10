1c0a286ef846725b4818bf1b557a0fd81c64e55c
CJ-404 <cj.ptsz@yahoo.com>
2024-08-10 14:38:06 +0530

    feat: add overlay dialog to show list of shared files for a project

--- lib/screens/view_project_screen.dart
+++ lib/screens/view_project_screen.dart
@@ -2,11 +2,14 @@ import 'package:flatwork/config/config.dart';
 import 'package:flatwork/data/data.dart';
 import 'package:flatwork/providers/providers.dart';
 import 'package:flatwork/utils/utils.dart';
+import 'package:flatwork/widgets/display_list_of_shared_files.dart';
 import 'package:flatwork/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../services/auth_services.dart';
 
 class ViewProjectScreen extends ConsumerWidget {
   static ViewProjectScreen builder(BuildContext context, GoRouterState state , String projectId)
@@ -38,12 +41,24 @@ class ViewProjectScreen extends ConsumerWidget {
                 return Column(
                   children: [
                     Container(
-                      height: deviceSize.height*0.3,
+                      height: deviceSize.height*0.35,
                       width: deviceSize.width,
                       color: colors.secondary,
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              IconButton(
+                                icon: const Icon(Icons.circle, color: Colors.green,size: 60),
+                                onPressed: () {
+                                  ref.read(authProvider.notifier).logout();
+                                  context.pushNamed(RouteLocation.login);
+                                },
+                              ),
+                            ],
+                          ),
                           DisplayWhiteText(
                               text: project.title,
                               fontSize: 32
@@ -51,6 +66,17 @@ class ViewProjectScreen extends ConsumerWidget {
                           const DisplayWhiteText(
                               text: 'Task List',
                               fontSize: 25
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              IconButton(
+                                icon: Icon(Icons.edit_document, color: colors.onPrimary,size: 30,),
+                                onPressed: () {
+                                  showOverlayDialog(context, ref);
+                                },
+                              ),
+                            ],
                           ),
                         ],
                       ),
@@ -64,7 +90,7 @@ class ViewProjectScreen extends ConsumerWidget {
               ),
           ),
           Positioned(
-              top: 130,
+              top: 200,
               left: 0,
               right: 0,
               child: SafeArea(
@@ -116,6 +142,70 @@ class ViewProjectScreen extends ConsumerWidget {
           ),
         ],
       ),
+    );
+  }
+
+  void showOverlayDialog(BuildContext context, WidgetRef ref) async {
+    final userRole = await AuthServices().getSavedUserRole();
+    final colors = context.colorScheme;
+    showDialog(
+      context: context,
+      barrierDismissible: true, // dismiss when tapping outside
+      builder: (BuildContext context) {
+        return Dialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          elevation: 16,
+          child: Container(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                (userRole == "manager")?
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'Shared project files',
+                      style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.add, color: colors.primary,size: 30,),
+                      onPressed: () {
+                      },
+                    ),
+                  ],
+                )
+                    :
+                const Text(
+                  'Shared project files',
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                ),
+                const Divider(thickness: 1.5,),
+                //list of shared files
+                SingleChildScrollView(
+                  physics: const AlwaysScrollableScrollPhysics(),
+                  // padding: const EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      DisplayListOfSharedFiles(sharedFiles: const ["asa","fathead"], ref: ref),
+                    ],
+                  ),
+                ),
+                // ElevatedButton(
+                //   onPressed: () {
+                //     Navigator.of(context).pop(); // Close the dialog
+                //   },
+                //   child: const Text('Close'),
+                // ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
