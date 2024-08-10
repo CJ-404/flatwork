fc49f86e14c582f4db8a154d4c490efac8259fd9
CJ-404 <cj.ptsz@yahoo.com>
2024-08-10 14:35:53 +0530

    create: shred file tile for show shared project and taask files

---
+++ lib/widgets/shared_file_tile.dart
@@ -0,0 +1,40 @@
+import 'package:flatwork/data/data.dart';
+import 'package:flatwork/utils/utils.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+class SharedFileTile extends StatelessWidget {
+  const SharedFileTile({super.key, required this.fileName});
+
+  final String fileName;
+
+  @override
+  Widget build(BuildContext context) {
+    final style = context.textTheme;
+    final colors = context.colorScheme;
+
+    return Row(
+      children: [
+        const Gap(16),
+        Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  fileName,
+                  style: style.titleMedium?.copyWith(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 20,
+                  ),
+                ),
+              ],
+            )),
+        IconButton(
+          icon: Icon(Icons.download, color: colors.primary,size: 25,),
+          onPressed: () {
+          },
+        ),
+      ],
+    );
+  }
+}
