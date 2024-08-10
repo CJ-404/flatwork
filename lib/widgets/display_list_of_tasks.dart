624d681c744dffb4c6037cb42fe7565072c5dafe
CJ-404 <cj.ptsz@yahoo.com>
2024-08-11 03:15:13 +0530

    fix: responsiveness when user role is different

--- lib/widgets/display_list_of_tasks.dart
+++ lib/widgets/display_list_of_tasks.dart
@@ -7,6 +7,8 @@ import '../data/data.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../services/auth_services.dart';
+
 class DisplayListOfTasks extends StatelessWidget {
   const DisplayListOfTasks({
     super.key,
@@ -22,43 +24,59 @@ class DisplayListOfTasks extends StatelessWidget {
     final deviceSize = context.deviceSize;
     const emptyTasksMessage = "There are no tasks yet";
 
-    return CommonContainer(
-      height: deviceSize.height*0.68,
-      color: context.colorScheme.onPrimary,
-      child: tasks.isEmpty?
-      Center(
-        child: Text(
-          emptyTasksMessage,
-          style: context.textTheme.headlineSmall,
-        ),
-      )
-          :
-      ListView.separated(
-        shrinkWrap: true,
-        itemCount: tasks.length,
-        itemBuilder: (ctx, index) {
-          final task = tasks[index];
-
-          return InkWell(
-              onLongPress: () {
-                //TODO: Delete Task
-              },
-              onTap: () {
-                ref.read(taskIdProvider.notifier).state = task.id.toString();
-                context.pushNamed(
-                  RouteLocation.editTask,
-                  pathParameters: {'taskId': task.id.toString()},
-                );
-              },
-              child: TaskTile(
-                  task: task,
-                  onCompleted: (value){},
+    return FutureBuilder<String>(
+        future: AuthServices().getSavedUserRole(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+          // } else if (!snapshot.hasData || snapshot.data!['token'] == null) {
+          //   return Center(child: Text('User data not found.'));
+          } else {
+            final userRole = snapshot.data!;
+            return CommonContainer(
+              height: (userRole == "manager")? deviceSize.height * 0.56 : deviceSize.height * 0.68,
+              color: context.colorScheme.onPrimary,
+              child: tasks.isEmpty ?
+              Center(
+                child: Text(
+                  emptyTasksMessage,
+                  style: context.textTheme.headlineSmall,
+                ),
               )
-          );
-        }, separatorBuilder: (BuildContext context, int index) {
-        return const Divider(thickness: 1.5,);
-      },
-      ),
+                  :
+              ListView.separated(
+                shrinkWrap: true,
+                itemCount: tasks.length,
+                itemBuilder: (ctx, index) {
+                  final task = tasks[index];
+
+                  return InkWell(
+                      onLongPress: () {
+                        //TODO: Delete Task
+                      },
+                      onTap: () {
+                        ref
+                            .read(taskIdProvider.notifier)
+                            .state = task.id.toString();
+                        context.pushNamed(
+                          RouteLocation.editTask,
+                          pathParameters: {'taskId': task.id.toString()},
+                        );
+                      },
+                      child: TaskTile(
+                        task: task,
+                        onCompleted: (value) {},
+                      )
+                  );
+                }, separatorBuilder: (BuildContext context, int index) {
+                return const Divider(thickness: 1.5,);
+              },
+              ),
+            );
+          }
+        }
     );
   }
 }
