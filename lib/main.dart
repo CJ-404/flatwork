9c134372fbe87b0462ef34a55fce2a73e5bbb8be
CJ-404 <cj.ptsz@yahoo.com>
2024-08-11 05:57:58 +0530

    fix: firebase connection and authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'dart:async';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flatwork/app/flatwork_app.dart';
+import 'package:flatwork/firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+Future<void> main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(
     const ProviderScope(
       child: FlatWorkApp(),
