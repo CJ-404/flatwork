ed349f468a89be3561e7e94650f32083eaa6363d
CJ-404 <cj.ptsz@yahoo.com>
2024-11-27 20:51:09 +0530

    fix: reset bottom nav index on startup

--- lib/config/routes/routes_provider.dart
+++ lib/config/routes/routes_provider.dart
@@ -3,16 +3,23 @@ import 'package:flatwork/services/api_services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flatwork/providers/providers.dart';
+import '../../widgets/main_scaffold.dart';
 
 final routesProvider = Provider<GoRouter>(
   (ref) {
     final authState = ref.watch(authProvider);
-    // set the token to pi service
+    String location = RouteLocation.login;
+
+    // reset bottom navigation index state
+    ref.read(bottomNavigationIndexProvider.notifier).state = 0;
+
     if (authState.isAuthenticated) {
+      // set the token ato pi service
       ApiServices.accessToken = authState.token;
+      location = RouteLocation.home;
     }
     return GoRouter(
-      initialLocation: authState.isAuthenticated? RouteLocation.home : RouteLocation.login,
+      initialLocation: location,
       navigatorKey: navigationKey,
       routes: appRoutes,
     );
